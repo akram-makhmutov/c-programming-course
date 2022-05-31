31ce3ea7f269e1e93beebafb0dcb73cf564ffb85
Akram <akr232009@gmail.com>
2022-05-31 21:01:12 +0600

    upd

---
+++ space.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+void main()
+{
+    char symba = '\0';
+    while (cin.get(symba)) {
+        if  ( (symba == ' ') && (cin.peek() == ' ') ) continue;
+        cout<<symba;
+    }
+}
