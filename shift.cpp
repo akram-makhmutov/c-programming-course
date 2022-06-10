6594bca8dcc465ebb4d66b38ab0d2c3f12c4a9b0
Akram <akr232009@gmail.com>
2022-06-10 14:18:36 +0600

    upd

--- shift.cpp
+++ shift.cpp
@@ -1,7 +1,7 @@
 void rotate(int a[], unsigned size, int shift) {
     int gt;
     if (shift > size) shift = shift - size;
-    for (int i = 0; i < shift; i++) {
+    for (int *p = a; p < a + shift; p++) {
         for (int j = 0; j < size - 1; j++) {
             gt = a[j];
             a[j] = a[j+1];
