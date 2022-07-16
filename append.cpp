b6593e227808544798a0fe6c591e500a87c4b904
Akram <akr232009@gmail.com>
2022-07-16 10:17:02 +0600

    upd

---
+++ append.cpp
@@ -0,0 +1,19 @@
+#include <cstddef> // size_t
+#include <cstring> // strlen, strcpy
+
+struct String {
+	String(const char *str = "");
+	String(size_t n, char c);
+	~String();
+	void append(String &other) {
+	    size_t sex = size + other.size;
+	    char *new_str = new char [sex + 1];
+        strcpy (new_str, str);
+        strcat (new_str, other.str);
+        delete [] str;
+        str = new_str;
+        size = strlen(str);
+	}
+	size_t size;
+	char *str;
+};
